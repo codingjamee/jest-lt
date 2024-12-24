88ce20220c02e083d9e67be38192727ce9c585b7
허제인 <codingjamee@gmail.com>
2024-12-25 00:03:14 +0900

    toHaveBeenCalled toHaveBeenCalledWith, toHaveBeenCalledTimes, spyOn메서드 spyOn은 obj자체에 spy를 심어 자체에서 toHaveBeenCalledTimes를 호출할 수 있게

---
+++ src/toHaveBeenCalled.spec.ts
@@ -0,0 +1,30 @@
+import { sum, obj } from "./toBe";
+
+test("sum 함수가 호출 되었는가.", () => {
+  const sumSpy = jest.fn(sum); //jest.fn으로 함수를 만들어 호출 여부, 횟수등 확인 가능
+  sumSpy(1, 2);
+  expect(sumSpy).toHaveBeenCalled(); //테스트시 의미가 없음
+});
+
+test("sum 함수가 1 번 호출 되었다.", () => {
+  const sumSpy = jest.fn(sum);
+  sumSpy(1, 2);
+  expect(sumSpy).toHaveBeenCalledTimes(1);
+});
+test("sum 함수가 1,2와 함께 호출 되었다.", () => {
+  const sumSpy = jest.fn(sum);
+  sumSpy(1, 2);
+  expect(sumSpy).toHaveBeenCalledWith(1, 2);
+});
+
+test("obj 함수가 1 번 호출 되었다.", () => {
+  const objSpy = jest.fn(obj.minus);
+  objSpy(1, 2);
+  expect(objSpy).toHaveBeenCalledTimes(1);
+});
+
+test("obj 함수가 1,2와 함꼐 호출 되었다.", () => {
+  jest.spyOn(obj, "minus");
+  obj.minus(1, 2);
+  expect(obj.minus).toHaveBeenCalledTimes(1);
+});
