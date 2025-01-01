ab1dc8de75f106a34058e1d071263619cc3a5168
허제인 <codingjamee@gmail.com>
2025-01-02 00:02:42 +0900

    웬만해서는 callback함수 쓰지 않도록 promise로 설정

--- src/callback.ts
+++ src/callback.ts
@@ -1,3 +1,11 @@
 export function timer(callback) {
   setTimeout(() => callback("success"), 3000);
 }
+
+export function timerPromise() {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res("success");
+    }, 3000);
+  });
+}
