fe9f45de209a022f2c740e451b083372261cfd4f
허제인 <codingjamee@gmail.com>
2025-01-02 00:32:27 +0900

    에러 try catch활용하여 테스트

--- src/throwFunction.test.ts
+++ src/throwFunction.test.ts
@@ -1,5 +1,14 @@
-import { error, customError } from "./throwFunction";
+import { error, customError, CustomError } from "./throwFunction";
 
 test("error가 잘 나는가", () => {
   expect(() => error()).toThrow(Error);
+  expect(() => customError()).toThrow(CustomError);
+});
+
+test("error가 잘 나는가 try, catch활용", () => {
+  try {
+    error();
+  } catch (err) {
+    expect(err).toStrictEqual(new Error());
+  }
 });
--- src/throwFunction.ts
+++ src/throwFunction.ts
@@ -2,7 +2,7 @@ export function error() {
   throw new Error();
 }
 
-class CustomError extends Error {}
+export class CustomError extends Error {}
 
 export function customError() {
   throw new CustomError();
