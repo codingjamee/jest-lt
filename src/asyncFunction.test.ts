3cb89dea6c34d040b593a2f8647aad63a2a57693
허제인 <codingjamee@gmail.com>
2025-01-01 23:28:14 +0900

    전체 함수 네임스페이스로 import

--- src/asyncFunction.test.ts
+++ src/asyncFunction.test.ts
@@ -1,36 +1,36 @@
-import { okPromise, noPromise, noAsync, okAsync } from "./asyncFunction";
+import * as fns from "./asyncFunction";
 
 //promise리턴하는 함수 테스트하기 //resolves or rejects를 하면 반드시 return을 붙여야 함
 test("okPromise 테스트", () => {
-  return expect(okPromise()).resolves.toBe("ok");
+  return expect(fns.okPromise()).resolves.toBe("ok");
 });
 
 test("okPromise테스트 then", () => {
-  const okSpy = jest.fn(okPromise);
+  const okSpy = jest.fn(fns.okPromise);
   return okSpy().then((result) => {
     expect(result).toBe("ok");
   });
 });
 
 test("okPromise await 테스트", async () => {
-  const okSpy = jest.fn(okPromise);
+  const okSpy = jest.fn(fns.okPromise);
   const result = await okSpy();
   expect(result).toBe("ok");
 });
 
 test("noPromise테스트", () => {
-  return expect(noPromise()).rejects.toBe("no");
+  return expect(fns.noPromise()).rejects.toBe("no");
 });
 
 test("noPromise테스트 then", () => {
-  const noSpy = jest.fn(noPromise);
+  const noSpy = jest.fn(fns.noPromise);
   return noSpy().catch((result) => {
     expect(result).toBe("no");
   });
 });
 
 test("noPromise await 테스트", async () => {
-  const noSpy = jest.fn(noPromise);
+  const noSpy = jest.fn(fns.noPromise);
   try {
     await noSpy();
   } catch (err) {
