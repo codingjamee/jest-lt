bf0a7481b5d2ead10f177676ca921bae78fee449
허제인 <codingjamee@gmail.com>
2025-01-01 23:58:49 +0900

    callback함수는 done으로 실행해야 test를 setTimeout의 n초 후 정상적으로 할 수 있음

--- src/callback.test.ts
+++ src/callback.test.ts
@@ -1,7 +1,8 @@
 import { timer } from "./callback";
 
-test("timer 잘 실행되는가", () => {
+test("timer 잘 실행되는가", (done) => {
   timer((message: string) => {
     expect(message).toBe("success");
+    done();
   });
 });
