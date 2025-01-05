bc0b7ff1a30aec56e16a4ef733627ee5c6bf6b18
허제인 <codingjamee@gmail.com>
2025-01-05 22:12:56 +0900

    requireActual jest.mock jest.requireActual는 파일 실제 return부분 가져올 수 있음 jest.mock은 두번째 인수에 아무것도 넣지 않으면 전체 모듈을 모킹 가능

---
+++ src/module.test.ts
@@ -0,0 +1,23 @@
+import { obj } from "./module";
+
+// jest.mock("./module", () => {
+//   return { obj: { a: "b" }, prop: "hello" }; //obj를 갈아끼움
+// }); //module파일의 메서드 통째로 변경
+
+jest.mock("./module", () => {
+  return {
+    ...jest.requireActual("./module"),
+    obj: {
+      ...jest.requireActual("./module"),
+      method3() {
+        return "method3";
+      },
+    },
+  };
+});
+//requireActual : 나머지는 전체 원래 함수
+//method3만 모킹
+test("모듈을 전부 모킹", () => {
+  console.log(obj);
+  // jest.spyOn(obj, "method1");
+});
---
+++ src/module.ts
@@ -0,0 +1,15 @@
+export const obj = {
+  method1() {
+    return "123";
+  },
+  method2() {
+    return "123";
+  },
+  method3() {
+    return "123";
+  },
+  method4() {
+    return "123";
+  },
+  prop: "change me",
+};
